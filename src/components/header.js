8dcd67698b931bc3a3b40e3ba97e7b917c018f76
Will <will.k.01@live.co.uk>
2021-08-01 01:23:50 +0100

    Fixed header & build issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,42 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Header = () => {
     return (
         <header>
             <div className="bg-white shadow dark:bg-gray-800">
                 <div className="container flex items-center justify-between p-6 mx-auto font-mono font-bold text-gray-600 dark:text-gray-300">
-                    <a href="/" class="text-2xl">
-                        <h1 class="bg-rose-400 px-4 py-3 text-white">WK</h1>
-                    </a>
+                    <Link
+                        to="/"
+                        className="text-2xl bg-rose-400 px-4 py-3 text-white"
+                    >
+                        WK
+                    </Link>
 
                     <nav>
-                        <a
-                            href="/"
-                            className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+                        <Link
+                            to="/"
+                            className="border-b-2 hover:border-blue-500 mx-1.5 sm:mx-6"
+                            activeClassName="text-blue-500 border-blue-500"
                         >
                             home
-                        </a>
+                        </Link>
 
-                        <a
-                            href="/"
-                            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+                        <Link
+                            to="/cv"
+                            className="border-b-2 hover:border-blue-500 mx-1.5 sm:mx-6"
+                            activeClassName="text-blue-500 border-blue-500"
                         >
                             cv
-                        </a>
+                        </Link>
 
-                        <a
-                            href="/"
-                            className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+                        <Link
+                            to="/contact"
+                            className="border-b-2 hover:border-blue-500 mx-1.5 sm:mx-6"
+                            activeClassName="text-blue-500 border-blue-500"
                         >
                             contact
-                        </a>
+                        </Link>
                     </nav>
                 </div>
             </div>
