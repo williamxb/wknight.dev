80e1fcba0107817b73a887767a9850344d2cc763
Will <will.k.01@live.co.uk>
2021-08-09 01:51:20 +0100

    Updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,29 +4,32 @@ const tailwindConfig = require("./tailwind.config.js")
 const { theme } = resolveConfig(tailwindConfig)
 
 module.exports = {
-  siteMetadata: {
-    title: "Will Knight - Web Developer",
-    author: "Will Knight",
-    description: "A Gatsby starter to set you up with Tailwind CSS",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "Will Knight - Web Developer",
-        short_name: "Will Knight - Web Developer",
-        start_url: "/",
-        background_color: theme.colors.white,
-        theme_color: theme.colors.blue[500],
-        icon: "static/icon.svg",
-      },
+    siteMetadata: {
+        title: 'Will Knight - Web Developer',
+        author: 'Will Knight',
+        description: 'My Development Portfolio',
     },
-    {
-      resolve: "gatsby-plugin-postcss",
-      options: {
-        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
-      },
-    },
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'Will Knight - Web Developer',
+                short_name: "A Front-end Web Developer's Portfolio",
+                start_url: '/',
+                background_color: theme.colors.white,
+                theme_color: theme.colors.teal[500],
+                icon: 'static/icon.svg',
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-postcss',
+            options: {
+                postCssPlugins: [
+                    require('tailwindcss'),
+                    require('autoprefixer'),
+                ],
+            },
+        },
+    ],
 }
